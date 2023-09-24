1fe15515eb4f0d8fb46af0cd39378ac98d3306ac
unknown <akashguna2018@gmail.com>
2023-09-24 13:21:04 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Todo />
+</template>
+<script>
+import Todo from "./components/Todo.vue";
+export default {
+  name: "app",
+  data() {
+    return {};
+  },
+  computed: {},
+  components: {
+    Todo,
+  },
+
+  methods: {},
+};
+</script>
+<style></style>
